7676210b87790bf34d3fd349506d78da84cd1f21
Faiq Athif <100032449+fqathf@users.noreply.github.com>
2026-07-30 00:47:48 +0700

    fix bug double header page join us admin

--- src/app/admin/join-us/page.jsx
+++ src/app/admin/join-us/page.jsx
@@ -1,12 +1,7 @@
 'use client';
 
-import AdminLayout from '@/admin/AdminLayout';
 import AdminJoinUsPage from '@/admin/modules/join-us/AdminJoinUsPage';
 
 export default function AdminJoinUsRoute() {
-  return (
-    <AdminLayout>
-      <AdminJoinUsPage />
-    </AdminLayout>
-  );
+  return <AdminJoinUsPage />;
 }
